27ccc23711fd462b354d7a0297b94bbd4f2bc0ee
Naima Africa Francis <naima.africa@gmail.com>
2020-08-25 16:25:08 -0400

    Created Employees

---
+++ queries.sql
@@ -0,0 +1,42 @@
+Creating employees
+
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Justine Skye', '35000', 'Assitant Manager', '240', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Laurence "Lazy Larry" Williams', '16000', 'Cook', '249', 'Yes');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Sara Lee', '27000', 'Baker', '249', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Benjamin "Uncle Ben" Lamonte', '45000', 'Head Chef', '249', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Jeremiah Clean', '25000', 'Janitor', '242', 'Yes');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Francois Boyardee', '34000', 'Chef', '249', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Marie Callender', '61000', 'General Manager', '241', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Leann Wilds', '21000', 'Server', '0', 'Yes');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Alex Jimenez', '25000', 'Server', '0', 'No');
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition","PhoneExtension", "IsPartTime")
+VALUES(
+    'Jaime Tidewell', '23000', 'Busser', '0', 'Yes'); 
