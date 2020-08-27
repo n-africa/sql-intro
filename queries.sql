3e5137e26f47ff26d15c4c05ea283af36f5adcf2
Naima Africa Francis <naima.africa@gmail.com>
2020-08-27 17:00:02 -0400

    Updated for foreign keys homework

--- queries.sql
+++ queries.sql
@@ -64,6 +64,75 @@ delete from "Employees" where "FullName" like '%Lazy Larry%';
 
 
 alter table "Employees" add column "ParkingSpot" varchar
-(10);                                       
+(10);
 
+-- Foreign Keys Homework
+
+insert into "Departments"
+  ("DepartmentName" , "Building")
+values
+  ('Development', 'Main');
+insert into "Departments"
+  ("DepartmentName" , "Building")
+values
+  ('Marketing', 'North');
+
+
+insert into "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "Department
+ ID")
+values
+  ('Tim Smith', '40000', 'Programmer', '123', 'False', '1');
+insert into "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "Department
+ ID")
+values
+  ('Barbara Ramsey', '80000', 'Manager', '234', 'False', '1');
+insert into "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "Department
+ ID")
+values
+  ('Tom Jones', '32000', 'Admin', '456', 'True', '2');
+
+
+insert into "Products"
+  ("Price", "Name", "Description", "QuantityInStock")
+values
+  ('12.45', 'Widget', 'Th
+ e Original Widget', '100');
+insert into "Products"
+  ("Price", "Name", "Description", "QuantityInStock")
+values
+  ('99.99', 'Flowbee', 'P
+ erfect for haircuts', '3');
+
+
+insert into "Orders"
+  ("OrderNumber", "DatePlaced", "Email")
+values
+  ('X529', '1/1/2020 04:55 PM', 'person@
+ example.com');
+
+
+insert into "ProductOrders"
+  ("ID", "OrderID", "ProductID")
+values
+  ( 3, 1, 1);
+insert into "ProductOrders"
+  ("ID", "OrderID", "ProductID")
+values
+  ( 2, 1, 2);
+
+
+select *
+from "Employees" join "Departments" on "Employees"."DepartmentID" = "Departments"."ID"
+where "Departments"."DepartmentName" = 'Development'
+
+
+select *
+from "Orders" join "ProductOrders" ON "ProductOrders"."OrderID" = "Orders"."ID"
+where "ProductOrders"."ProductID" = 2
+
+
+delete from "ProductOrders" where "ProductID" = 2                                                   
 
